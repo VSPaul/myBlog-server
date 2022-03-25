08d3b727e60884be2bfc49413aff121a2647044d
VSPaul <vascupaul_vsp@yahoo.com>
2022-03-25 07:34:57 +0200

    fix cors

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/posts.controller");
 module.exports = (app) => {
     app.use((req, res, next) => {
         res.header(
+            ("Access-Control-Allow-Origin", true),"*",
             "Access-Controll-Allow-Headers",
             "x-access-token, Origin, Content-Type, Accept"
         );
