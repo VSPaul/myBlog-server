7e6da91802a754fcbc314ce134c864ee65f3c4bd
VSPaul <vascupaul_vsp@yahoo.com>
2022-03-25 07:36:55 +0200

    fix cors test

--- server.js
+++ server.js
@@ -8,17 +8,7 @@ const dotenv = require('dotenv').config();
 const app = express();
 app.use(express.static(__dirname));
 app.use(cookieParser());
-const whitelist = [`https://git.heroku.com/my-super-blog.git/getPosts`, `https://vspaul.github.io` ];
-app.use(cors({
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: true,
-}));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({
