c0029b247c1a61df92e24a7dc0a64a8a05790d86
VSPaul <vascupaul_vsp@yahoo.com>
2022-03-25 07:19:48 +0200

    fix route

--- server.js
+++ server.js
@@ -3,51 +3,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-// const db = require('./src/models');
 const dotenv = require('dotenv').config();
 
 const app = express();
 app.use(express.static(__dirname));
 app.use(cookieParser());
-app.use(cors())
-// app.use(cors({
-//     origin: process.env.COOKIE_PATH_ENV,
-//     credentials: true,
-// }));
+app.use(cors());
 
-// parse requests of content-type - application/json
-// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({
     verify: (req, res, buf) => {
         req.rawBody = buf;
     }
 }));
-// app.use(express.static("./src/salonPictures"));
-// app.use('/dashboard', express.static(__dirname));
 
 require('./src/routes/posts.routes')(app);
-// require('./src/routes/content.routes')(app);
-// require('./src/routes/stripe.routes')(app);
-// require('./src/routes/dashboard.routes')(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
-
-// db.sequelize
-//     .sync({ alter: true })
-//     .then(() =>
-//         app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
-//     );
-
-// db.sequelize
-//     .sync({ force: true })
-//     .then(() =>
-//         app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
-//     );
-
-// npx sequelize-auto -o ./src/models/ -d csyes -h localhost -u root -p 3306 -x Newbeginning888! -e mysql
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -5,7 +5,6 @@ module.exports = (app) => {
     app.use((req, res, next) => {
         res.header(
             "Access-Control-Allow-Origin",
-            ('Access-Control-Allow-Credentials', true),
             "Access-Controll-Allow-Headers",
             "x-access-token, Origin, Content-Type, Accept"
         );
