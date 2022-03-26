4953d6d6cc3c014afecaeca77a82c9d7e9e4b12c
VSPaul <vascupaul_vsp@yahoo.com>
2022-03-26 16:40:24 +0200

    Add/ crs

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/posts.controller");
 module.exports = (app) => {
     app.use((req, res, next) => {
         res.header(
-            ("Access-Control-Allow-Origin", "*"),
+            "Access-Control-Allow-Origin",
             ('Access-Control-Allow-Credentials', true),
             "Access-Controll-Allow-Headers",
             "x-access-token, Origin, Content-Type, Accept"
